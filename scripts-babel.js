9afb6b9a8454004c2dfc54a492e97cba9e825657
Ash Johns <ashleypjohns@gmail.com>
2017-08-26 21:37:27 -0400

    Added browser support
    Ran JS through Babel and CSS through CSSNext. Keeping ES6 files in the repo.

---
+++ scripts-babel.js
@@ -0,0 +1,77 @@
+'use strict';
+
+var text = document.getElementById('text');
+var morse = document.getElementById('morse');
+
+var morseLib = {
+  'A': '.-',
+  'B': '-...',
+  'C': '-.-.',
+  'D': '-..',
+  'E': '.',
+  'F': '..-.',
+  'G': '--.',
+  'H': '....',
+  'I': '..',
+  'J': '.---',
+  'K': '-.-',
+  'L': '.-..',
+  'M': '--',
+  'N': '-.',
+  'O': '---',
+  'P': '.--.',
+  'Q': '--.-',
+  'R': '.-.',
+  'S': '...',
+  'T': '-',
+  'U': '..-',
+  'V': '...-',
+  'W': '.--',
+  'X': '-..-',
+  'Y': '-.--',
+  'Z': '--..',
+  '1': '.----',
+  '2': '..---',
+  '3': '...--',
+  '4': '....-',
+  '5': '.....',
+  '6': '-....',
+  '7': '--...',
+  '8': '---..',
+  '9': '----.',
+  '0': '-----'
+};
+
+text.addEventListener('input', function () {
+  var output = '';
+  for (var i = 0; i < text.value.length; i++) {
+    if (text.value.charAt(i) == ' ') {
+      output += '   ';
+    } else {
+      input = text.value.charAt(i).toUpperCase();
+      output += morseLib[input] + " ";
+    }
+  }
+  morse.value = output;
+});
+
+morse.addEventListener('input', function () {
+  var output = '';
+  var char = '';
+
+  for (var i = 0; i < morse.value.length; i++) {
+    var current = morse.value.charAt(i);
+    if (current == '/') {
+      output += ' ';
+      char = '';
+    } else if (current == ' ') {
+      output += Object.keys(morseLib).find(function (key) {
+        return morseLib[key] == char;
+      });
+      char = '';
+    } else {
+      char += current;
+    }
+  }
+  text.value = output;
+});
